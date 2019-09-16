7069be42b9f4408f4a320998b4c5a08b2793c3e8
khielSantos <eziquielsantosoficial@gmail.com>
2019-09-16 00:20:10 -0300

    criou models e arquivos de rotas

---
+++ models/Professional.js
@@ -0,0 +1,141 @@
+const mongoose = require('mongoose');
+
+const ProfessionalSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  cellphone: {
+    type: [String]
+  },
+  skills: {
+    type: [String]
+  },
+  profession: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  cpf: {
+    type: String
+  },
+  street: {
+    type: String
+  },
+  cep: {
+    type: String
+  },
+  num: {
+    type: String
+  },
+  neighborhood: {
+    type: String
+  },
+  city: {
+    type: String
+  },
+  stade: {
+    type: String
+  },
+  evaluation: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+      },
+      noteValue: {
+        type: String
+      },
+      comment: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Professional = mongoose.model('profile', ProfessionalSchema);
