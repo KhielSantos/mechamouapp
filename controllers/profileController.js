b2dc0725beba1a4c21b41c4dc09cf20161237d6f
khielSantos <eziquielsantosoficial@gmail.com>
2019-09-17 10:13:28 -0300

    create profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -17,11 +17,95 @@ exports.getProfiles = async(req, res) => {
 exports.getProfileId = async(req, res) => { 
   try {
     const profile = await Profile.findOne({ _id: req.params.prof_id }).populate('user', ['name', 'avatar']);
-    console.log(profile)
     res.status(200).render('profile/profile', {
       profile
     });
   } catch (err) {
     res.status(500).json(err.response);  
   }
-}  
+}
+
+exports.getUserDashboard = async(req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.params.id}).select('-password').populate('user', ['name', 'avatar']);
+    console.log(profile)
+    res.status(200).render('dashboard/dashboard', {
+      profile
+    })
+  } catch (err) {
+    
+  }
+}
+
+exports.getCreateProfile = async (req, res) => {
+  res.status(200).render('profileForm/createProfile');
+}
+exports.postCreateProfile = async (req, res) => {
+
+  const {
+    cellphone,
+    profession,
+    description,
+    cpf,
+    skills,
+    street,
+    cep,
+    num,
+    neighborhood,
+    city,
+    stade,
+    note,
+    comment,
+    terms,
+    privacy,
+    twitter,
+    facebook,
+    linkedin,
+    youtube,
+    instagram
+  } = req.body;
+
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (description) profileFields.description = description;
+  if (profession) profileFields.profession = profession;
+  if (cpf) profileFields.cpf = cpf;
+  if (street) profileFields.street = street;
+  if (cep) profileFields.cep = cep;
+  if (num) profileFields.num = num;
+  if (neighborhood) profileFields.neighborhood = neighborhood;
+  if (city) profileFields.city = city;
+  if (stade) profileFields.stade = stade;
+  if (note) profileFields.note = note;
+  if (terms) profileFields.terms = terms;
+  if (comment) profileFields.comment = comment;
+  if (privacy) profileFields.privacy = privacy;
+  if (cellphone) {
+    profileFields.cellphone = cellphone
+      .split(',')
+      .map(cellphone => cellphone.trim());
+  }
+  if (skills) {
+    profileFields.skills = skills.split(',').map(skill => skill.trim());
+  }
+
+  profileFields.social = {};
+  if (youtube) profileFields.social.youtube = youtube;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+  if (instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOneAndUpdate(
+      { user: req.user.id },
+      { $set: profileFields },
+      { new: true, upsert: true }
+    );
+    res.status(200).render('dashboard/dashboard', {
+      profile
+    });
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+}
--- routes/profile.js
+++ routes/profile.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const profilesController = require('./../controllers/profileController');
 
 router.get('/', profilesController.getProfiles);
-router.get('/user/:prof_id', profilesController.getProfileId); 
+router.get('/user/:prof_id', profilesController.getProfileId);
+router.get('/profile/:id', profilesController.getUserDashboard);
+router.get('/create/profile', profilesController.getCreateProfile);
+router.post('/create/profile', profilesController.postCreateProfile);
 
 module.exports = router;
