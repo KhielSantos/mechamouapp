fb536b45b2e0d4f0a97608efbe620edafa6d27dd
Eziquiel Santos <41221370+KhielSantos@users.noreply.github.com>
2019-09-26 15:23:45 -0300

    Delete database.js

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV){
-  module.exports = {mongoURI: "mongodb+srv://khiel123:khiel123@me-chamou-lwpv0.mongodb.net/dbmechamou?retryWrites=true&w=majority"}
-}else{
-  module.exports = {mongoURI: "mongodb+srv://khiel123:khiel123@me-chamou-lwpv0.mongodb.net/dbmechamou?retryWrites=true&w=majority"}
-}
